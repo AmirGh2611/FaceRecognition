1289153e870fd0652745757a6f0421f5ee113724
amirhossein <amirabbasghorbani354@gmail.com>
2025-12-09 15:15:30 +0330

    debug

--- siamese_nn/training.py
+++ siamese_nn/training.py
@@ -4,8 +4,9 @@ from torch import optim
 from data_prep import train_loader, valid_loader
 import torch
 import matplotlib.pyplot as plt
+
 Net = ConvNet()
-loss_fn = nn.CrossEntropyLoss()
+loss_fn = nn.BCELoss()
 optimizer = optim.SGD(Net.parameters(), lr=0.2)
 epoch = 50
 loss_value = []
@@ -13,12 +14,15 @@ Net.train()
 for epoch in range(epoch):
     for images, labels in train_loader:
         optimizer.zero_grad()
-        output1 = Net(images[:, 0, :, :, :])
-        output2 = Net(images[:, 1, :, :, :])
-        outputs = torch.cdist(output1, output2, p=2)
+        img1 = images[:, 0, :, :, :]
+        img2 = images[:, 1, :, :, :]
+        output1 = Net(img1)
+        output2 = Net(img2)
+        outputs = torch.norm(output1 - output2, p=2, dim=1, keepdim=True) # euclidean distance
         loss = loss_fn(outputs, labels)
         loss_value.append(loss.item())
-        print(f"epoch:{} ")
+        print(f"epoch:{epoch}, loss:{loss.item()} ")
         loss.backward()
+        optimizer.step()
 plt.plot(loss_value)
 plt.show()
