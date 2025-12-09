d3c0247986110c2e4537e38e69dfd0883cf3b62c
amirhossein <amirabbasghorbani354@gmail.com>
2025-12-09 11:56:22 +0330

    creating custom conv net class

--- siamese_nn/model.py
+++ siamese_nn/model.py
@@ -1,9 +1,41 @@
 from torch import nn
-from torch import optim
-import torch.nn.functional as f
-from torchvision.models import resnet18
 
 
-class Net(nn.Module):
+class ConvNet(nn.Module):
     def __init__(self):
-        super().__init__()
+        super(ConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=16),
+            nn.ReLU(),
+            nn.AdaptiveMaxPool2d((112, 112))
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=32),
+            nn.ReLU(),
+            nn.AdaptiveMaxPool2d((64, 64))
+        )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(),
+            nn.AdaptiveMaxPool2d((32, 32))
+        )
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(),
+            nn.AdaptiveMaxPool2d((16, 16))
+        )
+        self.layer5 = nn.Sequential(
+            nn.Flatten()
+        )
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        return out
